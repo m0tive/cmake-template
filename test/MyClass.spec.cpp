9fca2113425b6a739f54db075f674ea39364c37e
Peter Dodds <peter@pddds.com>
2017-04-03 21:51:38 +0100

    Bit of cleanup in the test

--- test/MyClass.spec.cpp
+++ test/MyClass.spec.cpp
@@ -1,17 +1,15 @@
 #include "catch.hpp"
 #include "../src/MyClass.hpp"
 
-#define ACCESSOR(Name, Worker, Class, Member) \
-    class Accessor_ ## Member : public Class { \
-        public: using Class::Member; \
-    }; \
-    auto Name = static_cast<Accessor_ ## Member *>(&Worker);
+#define MAKE_ACCESSOR(Name, Inst, Class, Member) \
+    struct Class ## _public_ ## Member : public Class { using Class::Member; }; \
+    auto Name = static_cast<Class ## _public_ ## Member *>(Inst);
 
 TEST_CASE("MyClass Spec") {
     MyClass inst(42);
 
     SECTION("ctor sets size") {
-        ACCESSOR(inst_access, inst, MyClass, m_size);
-        REQUIRE(inst_access->m_size == 42);
+        MAKE_ACCESSOR(instPtr, &inst, MyClass, m_size);
+        REQUIRE(instPtr->m_size == 42);
     }
 }
